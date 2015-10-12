aa8cea4b267f4ad9f4d7a2759790abb984597b5a
Rich Jenks <rich@richjenks.com>
2015-10-12 12:32:28 +0100

    Can force options with a config file

--- plugin/Admin.php
+++ plugin/Admin.php
@@ -6,6 +6,25 @@
 class Admin {
 
 	/**
+	 * @var bool Whether options should be taken from config file
+	 */
+	private $readonly;
+
+	/**
+	 * Handles options via config file
+	 */
+	public function __construct() {
+		$this->readonly = ( file_exists(__DIR__ . '/Config.php' ) ) ? true : false;
+		if ( $this->readonly ) {
+			$config = require 'Config.php';
+			update_option( 'marketo_pro_client_id',     $config['client_id'] );
+			update_option( 'marketo_pro_client_secret', $config['client_secret'] );
+			update_option( 'marketo_pro_munchkin_id',   $config['munchkin_id'] );
+			update_option( 'marketo_pro_marketo_id',    $config['marketo_id'] );
+		}
+	}
+
+	/**
 	 * Called by admin menu hook
 	 */
 	public function add_menus() {
@@ -17,6 +36,8 @@ class Admin {
 	 * Bootstraps the admin page, called by `add_menu_page`
 	 */
 	public function admin() {
+		$data['readonly']     = get_option('marketo_pro_client_id');
+
 		$data['client_id']     = get_option('marketo_pro_client_id');
 		$data['client_secret'] = get_option('marketo_pro_client_secret');
 		$data['munchkin_id']   = get_option('marketo_pro_munchkin_id');
